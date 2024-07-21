559c0c5daab78164e191d8d5f4d8cebfe2bfb044
sahilshetty10 <65429226+sahilshetty10@users.noreply.github.com>
2024-07-20 23:35:14 -0400

    Update NavBar.jsx

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -33,7 +33,7 @@ export default function NavBar({ sectionRefs }) {
     sectionRefs.forEach((section) => {
       ScrollTrigger.create({
         trigger: section,
-        start: "top 375px",
+        start: "top middle",
         end: "bottom 300px",
         // markers: true,
         animation: gsap
